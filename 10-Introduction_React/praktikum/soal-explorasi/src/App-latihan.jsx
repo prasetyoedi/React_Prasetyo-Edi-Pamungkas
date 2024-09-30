6f8b38e1d39b899bdaae5cf07bfa98426523493f
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-09-30 19:32:14 +0700

    up instalasi react

---
+++ 10-Introduction_React/praktikum/soal-explorasi/src/App-latihan.jsx
@@ -0,0 +1,14 @@
+// dinamakan komponen
+import "./App.css";
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  // tempat logika dan tampilan
+  return (
+    <>
+      <LandingPage></LandingPage>
+    </>
+  );
+}
+
+export default App;
---
+++ 10-Introduction_React/praktikum/soal-explorasi/src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HeroSection from "../components/HeroSection";
+
+export default function LandingPage() {
+    return (
+        <>
+            <div>LandingPage</div>
+        </>
+    );
+}
